17d00fae88b98dc3c7faaa4cdaf039b9ee6bc263
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2018-09-22 16:35:57 -0300

    :|

--- generaExe.bat
+++ generaExe.bat
@@ -1,8 +1,9 @@
-C:\GnuWin32\bin\flex Lexico.l
+del ts.txt
+c:\GnuWin32\bin\flex Lexico.l
 pause
-C:\GnuWin32\bin\bison -dyv Sintactico.y
+c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-C:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
 pause
 pause
 Primera.exe prueba.txt
